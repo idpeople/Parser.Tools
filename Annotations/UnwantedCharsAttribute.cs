33745e368e7ac4fea97d2a7510ca1083a0410d01
LH <lh@idpeople.dk>
2023-07-05 12:24:56 +0200

    Initital commit

---
+++ Annotations/UnwantedCharsAttribute.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Parser.Tools.Annotations
+{
+    /// <summary>
+    /// Unwanted characters attribute for class
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Class)]
+    public sealed class UnwantedCharsAttribute : Attribute
+    {
+        /// <summary>
+        /// Array of unwanted characters
+        /// </summary>
+        public char[] UnwantedChars { get; private set; }
+
+        public UnwantedCharsAttribute(char[] unwantedChars)
+        {
+            UnwantedChars = unwantedChars;
+        }
+    }
+}
