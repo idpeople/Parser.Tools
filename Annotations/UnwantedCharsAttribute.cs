f9af3368b36207e55d81939d6515f6f4f6435766
LH <lh@idpeople.dk>
2023-07-06 09:17:39 +0200

    Added replacement string

--- Annotations/UnwantedCharsAttribute.cs
+++ Annotations/UnwantedCharsAttribute.cs
@@ -5,17 +5,17 @@ namespace Parser.Tools.Annotations
     /// <summary>
     /// Unwanted characters attribute for class
     /// </summary>
-    [AttributeUsage(AttributeTargets.Class)]
     public sealed class UnwantedCharsAttribute : Attribute
     {
         /// <summary>
         /// Array of unwanted characters
         /// </summary>
         public char[] UnwantedChars { get; private set; }
-
-        public UnwantedCharsAttribute(char[] unwantedChars)
+        public string Replacement { get; private set; }
+        public UnwantedCharsAttribute(char[] unwantedChars, string replacement="")
         {
             UnwantedChars = unwantedChars;
+            Replacement = replacement;
         }
     }
 }
